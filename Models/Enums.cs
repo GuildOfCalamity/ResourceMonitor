11e373045936475fa0d981f738509261cbb82c12
GuildOfCalamity <81915293+GuildOfCalamity@users.noreply.github.com>
2023-08-14 19:38:52 -0400

    added models folder

---
+++ Models/Enums.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Monitor;
+
+public enum eGraphType
+{
+    CPU = 1,
+    RAM = 2,
+    LAN = 3,
+    DISK = 4,
+}
---
+++ Models/Settings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monitor;
+
+public class Settings
+{
+    public string Theme { get; set; } = "Dark";
+    public double Frequency { get; set; } = 3.0;
+	public int PositionX { get; set; }
+    public int PositionY { get; set; }
+    public int WindowHeight { get; set; }
+    public int WindowWidth { get; set; }
+	public bool CenterScreen { get; set; } // RFU
+    public Settings() { }
+    public override string ToString()
+    {
+        return $"[CenterScreen: {CenterScreen}] [Width:{WindowWidth}] [Height:{WindowHeight}] [X:{PositionX}] [Y:{PositionY}]";
+    }
+}
